853e31e9298ec5caedbfce79433463afc11f85c8
Carlos <miklo.concepcion@gmail.com>
2019-12-17 17:33:07 -0600

    first commit already breaking things. debugging. :)

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
+const express = require("express");
 
-const db = require('./data/dbConfig.js');
+const db = require("./data/dbConfig.js");
 
 const server = express();
 
